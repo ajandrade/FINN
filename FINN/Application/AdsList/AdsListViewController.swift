ac27e796b5f9cfd9518e69598cc8b402163c26f4
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-04 15:20:10 +0000

    Added collection view boilerplate code

--- FINN/Application/AdsList/AdsListViewController.swift
+++ FINN/Application/AdsList/AdsListViewController.swift
@@ -17,15 +17,44 @@ class AdsListViewController: UIViewController {
   // MARK: - IBOUTLETS
   
   @IBOutlet weak var collectionView: UICollectionView! {
-    didSet {
-      collectionView.register(AdCell.self)
-    }
+    didSet { collectionView.register(AdCell.self) }
   }
   
   // MARK: - VIEW LIFE CYCLE
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
+}
+
+extension AdsListViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 30
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueCell(for: indexPath) as AdCell
+    return cell
+  }
+  
+}
+
+extension AdsListViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 100, height: 100)
+  }
+}
+
+extension AdsListViewController: UICollectionViewDataSourcePrefetching {
+  
+  func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+    
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+    
+  }
 }
