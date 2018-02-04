b9040c6cc639548e136ee76bf92e9298feff7bf5
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-04 15:11:20 +0000

    Added AdCell

---
+++ FINN/Application/AdsList/Cells/AdCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdCell.swift
+//  FINN
+//
+//  Created by Amadeu Andrade on 04/02/2018.
+//  Copyright © 2018 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class AdCell: UICollectionViewCell {
+  
+  // MARK: - INITIALIZATION
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  // MARK: - CONFIGURATION
+  
+  func configure(with presenter: AdCellPresenterRepresentable) { }
+  
+}
