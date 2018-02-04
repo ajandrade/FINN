1208eec1eacd702706dca17653f16b4cbf802a26
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-04 13:55:12 +0000

    Set initial folder structure

---
+++ FINN/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FINN
+//
+//  Created by Amadeu Andrade on 04/02/2018.
+//  Copyright © 2018 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
+
