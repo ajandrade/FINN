ab29adbdb81d6fe02205333d973cdca237073b68
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-09 18:18:21 +0000

    Added file manager tests

---
+++ FINNTests/Test Cases/Managers/FileManagerProviderTests.swift
@@ -0,0 +1,78 @@
+//
+//  FileManagerProviderTests.swift
+//  FINNTests
+//
+//  Created by Amadeu Andrade on 09/02/2018.
+//  Copyright © 2018 Amadeu Andrade. All rights reserved.
+//
+
+import XCTest
+@testable import FINN
+
+class FileManagerProviderTests: XCTestCase {
+  
+  var fileManager: FileManagerProvider!
+  var key: String!
+  var data: Data!
+
+  override func setUp() {
+    super.setUp()
+    fileManager = FileManagerProvider()
+    key = "testKey"
+    data = Data()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    fileManager.delete(key) { _ in }
+    fileManager = nil
+    key = nil
+    data = nil
+  }
+  
+  func testWriteAndLoadFile() {
+    let writeExpectation = expectation(description: "saved")
+    fileManager.write(data, for: key) { result in
+      switch result {
+      case .success:
+        self.fileManager.load(from: self.key, { result in
+          switch result {
+          case .success: writeExpectation.fulfill()
+          case .failure: XCTFail()
+          }
+        })
+      case .failure:
+        XCTFail()
+      }
+    }
+    wait(for: [writeExpectation], timeout: 1)
+  }
+  
+  func testDeleteFile() {
+    let deleteExpectation = expectation(description: "deleted")
+    // Write
+    fileManager.write(data, for: key) { result in
+      switch result {
+      case .success:
+        // Delete
+        self.fileManager.delete(self.key, { result in
+          switch result {
+          case .success:
+            // Check if exists
+            self.fileManager.load(from: self.key, { result in
+              switch result {
+              case .success: XCTFail()
+              case .failure: deleteExpectation.fulfill()
+              }
+            })
+          case .failure: XCTFail()
+          }
+        })
+      case .failure:
+        XCTFail()
+      }
+    }
+    wait(for: [deleteExpectation], timeout: 1)
+  }
+  
+}
