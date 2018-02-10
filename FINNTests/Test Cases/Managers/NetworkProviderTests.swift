faf884e2a1daf7d53fe5b8d0de74a2b8d6c001b6
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-10 12:47:14 +0000

    Finished network provider/request builder tests

--- FINNTests/Test Cases/Managers/NetworkProviderTests.swift
+++ FINNTests/Test Cases/Managers/NetworkProviderTests.swift
@@ -27,33 +27,6 @@ class NetworkProviderTests: XCTestCase {
     mockSession = nil
   }
   
-  func testAdsAPIBaseUrlIsCorrect() {
-    let correctBasePath = "https://gist.githubusercontent.com"
-    let adsBaseUrl = FINNAdsAPI.baseURLString
-    XCTAssertEqual(adsBaseUrl, correctBasePath)
-  }
-  
-  func testImageAPIBaseUrlIsCorrect() {
-    let correctBasePath = "https://images.finncdn.no"
-    let imagesBaseUrl = FINNImageAPI.baseURLString
-    XCTAssertEqual(imagesBaseUrl, correctBasePath)
-  }
-  
-  func testAdsRequestIsCorrect() {
-    let endpoint = "https://gist.githubusercontent.com/3lvis/3799feea005ed49942dcb56386ecec2b/raw/63249144485884d279d55f4f3907e37098f55c74/discover.json"
-    let urlRequest = URLRequest(url: URL(string: endpoint)!)
-    let endpointUrlRequest = RequestBuilder.build(for: .ads)
-    XCTAssertEqual(endpointUrlRequest, urlRequest)
-  }
-  
-  func testImageRequestIsCorrect() {
-    let someUri = "uri"
-    let endpoint = "https://images.finncdn.no/dynamic/480x360c/\(someUri)"
-    let urlRequest = URLRequest(url: URL(string: endpoint)!)
-    let endpointUrlRequest = RequestBuilder.build(for: .images(someUri))
-    XCTAssertEqual(endpointUrlRequest, urlRequest)
-  }
-  
   func testNoResponseErrorIsHandledForAds() {
     mockSession = MockURLSession(data: nil, urlResponse: nil, error: nil)
     network = NetworkProvider(session: mockSession)
---
+++ FINNTests/Test Cases/Managers/RequestBuilderTests.swift
@@ -0,0 +1,49 @@
+//
+//  RequestBuilderTests.swift
+//  FINNTests
+//
+//  Created by Amadeu Andrade on 10/02/2018.
+//  Copyright © 2018 Amadeu Andrade. All rights reserved.
+//
+
+import XCTest
+@testable import FINN
+
+class RequestBuilderTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testAdsAPIBaseUrlIsCorrect() {
+    let correctBasePath = "https://gist.githubusercontent.com"
+    let adsBaseUrl = FINNAdsAPI.baseURLString
+    XCTAssertEqual(adsBaseUrl, correctBasePath)
+  }
+  
+  func testImageAPIBaseUrlIsCorrect() {
+    let correctBasePath = "https://images.finncdn.no"
+    let imagesBaseUrl = FINNImageAPI.baseURLString
+    XCTAssertEqual(imagesBaseUrl, correctBasePath)
+  }
+  
+  func testAdsRequestIsCorrect() {
+    let endpoint = "https://gist.githubusercontent.com/3lvis/3799feea005ed49942dcb56386ecec2b/raw/63249144485884d279d55f4f3907e37098f55c74/discover.json"
+    let urlRequest = URLRequest(url: URL(string: endpoint)!)
+    let endpointUrlRequest = RequestBuilder.build(for: .ads)
+    XCTAssertEqual(endpointUrlRequest, urlRequest)
+  }
+  
+  func testImageRequestIsCorrect() {
+    let someUri = "uri"
+    let endpoint = "https://images.finncdn.no/dynamic/480x360c/\(someUri)"
+    let urlRequest = URLRequest(url: URL(string: endpoint)!)
+    let endpointUrlRequest = RequestBuilder.build(for: .images(someUri))
+    XCTAssertEqual(endpointUrlRequest, urlRequest)
+  }
+  
+}
